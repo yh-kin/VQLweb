092d13cf2a15f6412960991482b6da58724490eb
Yonghyun.cho <esoots1@gmail.com>
2015-04-11 22:14:21 +0900

    setting..

---
+++ VQLweb/src/main/java/controller/MainController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		model.addAttribute("main");
+		
+		return "home";
+	}
+}
