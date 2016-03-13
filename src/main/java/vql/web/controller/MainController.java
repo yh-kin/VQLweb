9ad5c8548c69c113801caed481ad6d02635cd267
cho-yonghyun <cho.yh.dev@gmail.com>
2016-03-13 23:57:23 +0900

    change version and build.gradle for archive name

--- src/main/java/vql/web/controller/MainController.java
+++ src/main/java/vql/web/controller/MainController.java
@@ -14,7 +14,7 @@ import vql.web.service.VisualizeService;
 @Controller
 public class MainController {
 	
-	private static final String VERSION = "1.0.1-SNAPSHOT";
+	private static final String VERSION = "1.0.1-SNAPSHOT 2";
 
 	/*
 	 * 추후 json -> javascript object로 변환시
